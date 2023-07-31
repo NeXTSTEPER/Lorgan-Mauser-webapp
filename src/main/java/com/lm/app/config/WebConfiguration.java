7ab3a1b5ebb216ff901b39890d3fbd0e413cadda
Alex Cox <116328901+NeXTSTEPER@users.noreply.github.com>
2023-07-30 21:44:43 -0500

    -

--- src/main/java/com/lm/app/config/WebConfiguration.java
+++ src/main/java/com/lm/app/config/WebConfiguration.java
@@ -4,7 +4,13 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -17,5 +23,18 @@ public class WebConfiguration implements WebMvcConfigurer {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-  
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);  // You can change this to suit your requirements
+        return slr;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
 }
--- src/main/java/com/lm/app/dao/OrderDAO.java
+++ src/main/java/com/lm/app/dao/OrderDAO.java
@@ -24,13 +24,15 @@ import com.lm.app.pagination.PaginationResult;
 @Transactional
 @Repository
 public class OrderDAO {
- 
+
     @Autowired
     private SessionFactory sessionFactory;
- 
+
     @Autowired
     private ProductDAO productDAO;
- 
+
+    private static final double SALES_TAX_RATE = 0.07; // assuming 7% sales tax
+
     private int getMaxOrderNum() {
         String sql = "Select max(o.orderNum) from " + Order.class.getName() + " o ";
         Session session = this.sessionFactory.getCurrentSession();
@@ -41,29 +43,41 @@ public class OrderDAO {
         }
         return value;
     }
- 
+
+    public void updateOrderAmountWithTax(String orderId) {
+        Session session = sessionFactory.getCurrentSession();
+        Order order = session.get(Order.class, orderId);
+        if (order != null) {
+            double totalAmount = order.getAmount();
+            double salesTax = totalAmount * SALES_TAX_RATE;
+            totalAmount += salesTax;
+            order.setAmount(totalAmount);
+            session.update(order);
+        }
+    }
+
     @Transactional(rollbackFor = Exception.class)
     public void saveOrder(CartInfo cartInfo) {
         Session session = this.sessionFactory.getCurrentSession();
- 
+
         int orderNum = this.getMaxOrderNum() + 1;
         Order order = new Order();
- 
+
         order.setId(UUID.randomUUID().toString());
         order.setOrderNum(orderNum);
         order.setOrderDate(new Date());
         order.setAmount(cartInfo.getAmountTotal());
- 
+
         CustomerInfo customerInfo = cartInfo.getCustomerInfo();
         order.setCustomerName(customerInfo.getName());
         order.setCustomerEmail(customerInfo.getEmail());
         order.setCustomerPhone(customerInfo.getPhone());
         order.setCustomerAddress(customerInfo.getAddress());
- 
+
         session.persist(order);
- 
+
         List<CartLineInfo> lines = cartInfo.getCartLines();
- 
+
         for (CartLineInfo line : lines) {
             OrderDetail detail = new OrderDetail();
             detail.setId(UUID.randomUUID().toString());
@@ -71,38 +85,40 @@ public class OrderDAO {
             detail.setAmount(line.getAmount());
             detail.setPrice(line.getProductInfo().getPrice());
             detail.setQuanity(line.getQuantity());
- 
+
             String code = line.getProductInfo().getCode();
             Product product = this.productDAO.findProduct(code);
             detail.setProduct(product);
- 
+
             session.persist(detail);
         }
- 
+
         // Order Number!
         cartInfo.setOrderNum(orderNum);
         // Flush
         session.flush();
+
+        // After saving the order, update the order amount with sales tax
+        updateOrderAmountWithTax(order.getId());
     }
- 
-    // @page = 1, 2, ...
+
     public PaginationResult<OrderInfo> listOrderInfo(int page, int maxResult, int maxNavigationPage) {
         String sql = "Select new " + OrderInfo.class.getName()//
                 + "(ord.id, ord.orderDate, ord.orderNum, ord.amount, "
                 + " ord.customerName, ord.customerAddress, ord.customerEmail, ord.customerPhone) " + " from "
                 + Order.class.getName() + " ord "//
                 + " order by ord.orderNum desc";
- 
+
         Session session = this.sessionFactory.getCurrentSession();
         Query<OrderInfo> query = session.createQuery(sql, OrderInfo.class);
         return new PaginationResult<OrderInfo>(query, page, maxResult, maxNavigationPage);
     }
- 
+
     public Order findOrder(String orderId) {
         Session session = this.sessionFactory.getCurrentSession();
         return session.find(Order.class, orderId);
     }
- 
+
     public OrderInfo getOrderInfo(String orderId) {
         Order order = this.findOrder(orderId);
         if (order == null) {
@@ -112,18 +128,17 @@ public class OrderDAO {
                 order.getOrderNum(), order.getAmount(), order.getCustomerName(), //
                 order.getCustomerAddress(), order.getCustomerEmail(), order.getCustomerPhone());
     }
- 
+
     public List<OrderDetailInfo> listOrderDetailInfos(String orderId) {
         String sql = "Select new " + OrderDetailInfo.class.getName() //
                 + "(d.id, d.product.code, d.product.name , d.quanity,d.price,d.amount) "//
                 + " from " + OrderDetail.class.getName() + " d "//
                 + " where d.order.id = :orderId ";
- 
+
         Session session = this.sessionFactory.getCurrentSession();
         Query<OrderDetailInfo> query = session.createQuery(sql, OrderDetailInfo.class);
         query.setParameter("orderId", orderId);
- 
+
         return query.getResultList();
     }
- 
 }
